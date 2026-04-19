5b19fde8129660b4d799307446e3cd58fa17e4d9
Masaya <masaya.nishimura0927@gmail.com>
2026-04-19 18:49:37 +0900

    infra層で、fetchRecipes(例外処理はまだ)とtoRecipeを実装

--- src/main/kotlin/com/example/kitchen_log_api/infrastructure/recipe/repository/RecipeCrudRepository.kt
+++ src/main/kotlin/com/example/kitchen_log_api/infrastructure/recipe/repository/RecipeCrudRepository.kt
@@ -1,6 +1,9 @@
 package com.example.kitchen_log_api.infrastructure.recipe.repository
 
 import com.example.kitchen_log_api.infrastructure.recipe.entity.RecipeEntity
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface RecipeCrudRepository: CoroutineCrudRepository<RecipeEntity, Long>
+interface RecipeCrudRepository: CoroutineCrudRepository<RecipeEntity, Long> {
+    fun findAllByUserId(userId: Long): Flow<RecipeEntity>
+}
