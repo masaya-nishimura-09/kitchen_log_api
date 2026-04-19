1ec890d5e5a4f4b443c330152724bd1cf04f66ab
Masaya <masaya.nishimura0927@gmail.com>
2026-04-19 11:28:20 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kitchen_log_api"
---
+++ src/main/kotlin/com/example/kitchen_log_api/KitchenLogApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kitchen_log_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KitchenLogApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KitchenLogApiApplication>(*args)
+}
