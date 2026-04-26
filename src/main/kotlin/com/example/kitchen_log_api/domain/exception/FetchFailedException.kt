6ad601fed7b07af0808adba988058977510c8a96
Masaya <masaya.nishimura0927@gmail.com>
2026-04-26 17:09:03 +0900

    カスタム例外を修正

--- src/main/kotlin/com/example/kitchen_log_api/domain/exception/FetchFailedException.kt
+++ src/main/kotlin/com/example/kitchen_log_api/domain/exception/FetchFailedException.kt
@@ -1,6 +1,6 @@
 package com.example.kitchen_log_api.domain.exception
 
-class FetchFailedException(
+open class FetchFailedException(
     message: String? = null,
     cause: Throwable? = null,
 ): KitchenLogApiException(message, cause)
